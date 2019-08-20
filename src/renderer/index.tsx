4197ddaecfb5a8102b6f9075e41d72b8ec47dc6b
remin <rdrgn@users.noreply.github.com>
2019-08-21 01:10:40 +0900

    update build commands and directory structure

---
+++ src/renderer/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+import { Options } from '../main/Options';
+import { App } from './App';
+import './index.scss';
+
+declare const getOptions: (() => Promise<Options>) | undefined;
+
+(async () => {
+  const options = getOptions !== undefined ? await getOptions() : {};
+  render(<App options={options} />, document.getElementById('root'));
+})().catch(reason => {
+  console.error(reason);
+});
