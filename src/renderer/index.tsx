a48b0dd152ae6ec76ee5447c80fc1cf64c4674ea
remin <noreply@remin.jp>
2019-09-10 01:51:27 +0900

    add a default graph for debugging

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -6,8 +6,13 @@ import './index.scss';
 
 declare const getOptions: (() => Promise<Options>) | undefined;
 
+const defaultGraph: Options = {
+  type: 'graph',
+  graph: { nodes: ['1', '2', '3'], edges: [[0, 1, '10'], [0, 2, '20'], [1, 2, '30']], directed: true },
+};
+
 (async () => {
-  const options = getOptions !== undefined ? await getOptions() : {};
+  const options: Options = typeof getOptions === 'function' ? await getOptions() : defaultGraph;
   if (options.graph !== undefined) {
     render(<GraphVisualizer graph={options.graph} />, document.getElementById('root'));
   }
